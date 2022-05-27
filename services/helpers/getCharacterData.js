43907d828a5da59d7b22895748b5e2d687142605
erayalkis <erayalkis@hotmail.com>
2022-05-27 21:45:37 +0300

    Create getCharacterData helper function

---
+++ services/helpers/getCharacterData.js
@@ -0,0 +1,38 @@
+const logger = require("pino")();
+const cheerio = require("cheerio");
+const parseGallery = require("./parseGallery");
+const parseProfile = require("./parseProfile");
+
+async function getCharacterData(URL, html, type) {
+  const $ = cheerio.load(html);
+
+  
+  // logger.warn(`HEADER IS ${$("ul.side-nav li.header").text()}`);
+
+  // if($("li.header").text() != "Character") {
+  //   logger.error(`${URL} is not a character profile or has custom CSS!`);
+  //   return {
+  //     error: 'The given ID belongs to a user profile or a profile with custom CSS'
+  //   }
+  // }
+
+  switch(type) {
+    case "gallery":
+      logger.info(`Parsing gallery for ${URL}`);
+      data = parseGallery($)
+      break;
+    case "details":
+      logger.info(`Parsing details for ${URL}`);
+      data = parseProfile($, false)
+      break;
+    default:
+      logger.info(`Parsing profile for ${URL}`);
+      data = parseProfile($)
+      break;
+  }
+
+  logger.info("Left switch statement");
+  return data;
+}
+
+module.exports = getCharacterData;
