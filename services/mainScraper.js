c27ab1df20792cc5d63c31ab77511ad9937c8479
erayalkis <erayalkis@hotmail.com>
2022-05-27 16:06:14 +0300

    Make fetchCharacter use async/await instead of .then()

--- services/mainScraper.js
+++ services/mainScraper.js
@@ -2,53 +2,49 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function fetchCharacter(id) {
+  const res = await axios.get(`https://toyhou.se/${id}`);
+  const $ = cheerio.load(res.data);
   
-  let charName = async () =>  axios.get(`https://toyhou.se/${id}`).then((res) => {
-    const $ = cheerio.load(res.data);
-    let info = $("div.profile-info-section");
-    let gallery = $("ul.magnific-gallery");
-    let char_tags = $("div.profile-tags-content");
+  let info = $("div.profile-info-section");
+  let gallery = $("ul.magnific-gallery");
+  let char_tags = $("div.profile-tags-content");
 
-    let name = $("h1.display-4").text();
-    let owner = { 
-      name: $("div.profile-name-info span.display-user").text(),
-      link: $("div.profile-name-info span.display-user a").attr("href")
-    }
-    let creator = {
-      name: info.find("span.display-user").text(),
-      link: info.find("span.display-user a").attr("href")
-    }
-    let desc = $("div.profile-content-content").text().replace(/\n|\r/g, "").trim();
+  let name = $("h1.display-4").text();
+  let owner = { 
+    name: $("div.profile-name-info span.display-user").text(),
+    link: $("div.profile-name-info span.display-user a").attr("href")
+  }
+  let creator = {
+    name: info.find("span.display-user").text(),
+    link: info.find("span.display-user a").attr("href")
+  }
+  let desc = $("div.profile-content-content").text().replace(/\n|\r/g, "").trim();
 
-    let recent_images = []
-    gallery.find("a.magnific-item").each((idx, ele) => {
-      recent_images.push(ele.attribs.href);
-    })
+  let recent_images = []
+  gallery.find("a.magnific-item").each((idx, ele) => {
+    recent_images.push(ele.attribs.href);
+  })
 
-    let tags = [];
+  let tags = [];
     char_tags.find("a.badge").each((idx, ele) => {
-      tags.push($(ele).text());
-    });
+    tags.push($(ele).text());
+   });
 
-    let fav_count = info.find("dl.fields div.fields-field").eq(2).find(".col-sm-8").text().trim();
-    let created_at = info.find("abbr.datetime").attr("title");
-    let created_n_ago = info.find("dl.fields div.fields-field").eq(0).find(".col-sm-8").text().trim();
+  let fav_count = info.find("dl.fields div.fields-field").eq(2).find(".col-sm-8").text().trim();
+  let created_at = info.find("abbr.datetime").attr("title");
+  let created_n_ago = info.find("dl.fields div.fields-field").eq(0).find(".col-sm-8").text().trim();
 
-    return {
-      name,
-      owner,
-      creator,
-      desc,
-      recent_images,
-      fav_count,
-      created_at,
-      created_n_ago,
-      tags
-    }
-  });
-
-  let char = await charName();
-  return char;
+  return {
+    name,
+    owner,
+    creator,
+    desc,
+    recent_images,
+    fav_count,
+    created_at,
+    created_n_ago,
+    tags
+  }
 }
 
 
