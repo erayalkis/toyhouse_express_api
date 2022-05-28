4ab7668fba6bf68339ac75ebbb6f0e9ef0f58860
erayalkis <erayalkis@hotmail.com>
2022-05-28 15:40:29 +0300

    Create test route for cookies

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const character = require("../routes/character");
+const getAuths = require("../models/helpers/getAuths");
 const logger = require("pino")();
 
 const APP_PORT = 8000;
+
 
 app.listen(APP_PORT, () => {
   logger.info(`Listening on http://localhost:${APP_PORT}/`);
@@ -13,5 +15,10 @@ app.listen(APP_PORT, () => {
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/auths_test", (req, res) => {
+  getAuths();
+  res.send("hi");
+})
 
 app.use("/character", character);
